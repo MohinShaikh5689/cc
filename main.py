3a995ee104ca05569ee1e1c0924dfdfbda8770c6
Mohin <ms9799630@gmail.com>
2026-04-14 18:05:53 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,14 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Hello, World!"
+
+@app.route("/about")
+def about():
+    return "This is the about page."
+
+if __name__ == "__main__":
+    app.run(host="127.0.0.1",port=8080, debug=True)
